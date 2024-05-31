7df5c2d052815939fb7e959d24bb08f7d5b273f9
subodh galande <subodh.dev@outlook.com>
2024-05-31 16:23:44 +0530

    code: exc - ebook download

---
+++ Object Oriented Javascript/EbookExcercise.js
@@ -0,0 +1,22 @@
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+}
+
+class Ebook extends Book {
+  constructor(title, author, year, fileSize) {
+    super(title, author, year);
+    this.fileSize = fileSize;
+  }
+
+  download() {
+    return `Title: ${this.title}  file-size: ${this.fileSize}MB`;
+  }
+}
+
+const EBook = new Ebook("Mybook", "goku", 2012, 12);
+
+console.log(EBook.download());
